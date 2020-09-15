024f9078d2ad2f113438f7a892b8b770b5807fd8
Valadzkoaliaksei <Valadzkoaliaksei@tut.by>
2020-09-15 23:02:59 +0300

    разбил на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+    <img className="header-img" src="images/english-language.png" />
+  </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img className="content-img" src="images/App-background.jpg" alt="App-background"></img>
+      </div>
+      <div className="user">
+        <img className="avatar" src="images/avatar1.jpg" alt="avatar"></img>
+        <p className="user-name">Valadzko Aliaksei</p>
+      </div>
+      <div>
+        My posts
+      <div>
+          New post
+      </div>
+        <div>
+          <div>
+            Post 1
+        </div>
+          <div>
+            Post 2
+        </div>
+          <div>
+            Post 3
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
